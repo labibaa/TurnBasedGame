b399aa80b4083f55ce1ac93a82011b483067fe46
Imtiaz Kabir <82272790+freeleech9@users.noreply.github.com>
2024-09-23 13:13:01 +0600

    mycharacter is dynamic

--- Assets/HoverDisplayStats.cs
+++ Assets/HoverDisplayStats.cs
@@ -1,43 +1,36 @@
 using UnityEngine;
-using UnityEngine.EventSystems;  // Needed for IPointerEnterHandler and IPointerExitHandler
-using TMPro;  // TextMesh Pro namespace
+using UnityEngine.EventSystems;
+using TMPro;
 
 public class HoverDisplayStats : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    public GameObject myCharacter;  // Assign your character in the inspector
-    public TextMeshProUGUI hpText;  // Assign the TextMesh Pro text component for HP
-    public TextMeshProUGUI apText;  // Assign the TextMesh Pro text component for AP
+    public GameObject myCharacter;  // Now this will be set from another script
+    public TextMeshProUGUI hpText;
+    public TextMeshProUGUI apText;
 
-    private TemporaryStats characterStats;  // To store the stats component
+    private TemporaryStats characterStats;
 
-    private void Start()
+    // Public method to update myCharacter from another script
+    public void SetCharacter(GameObject character)
     {
-        // Get the TemporaryStats component from the character
-        if (myCharacter != null)
-        {
-            characterStats = myCharacter.GetComponent<TemporaryStats>();
-        }
+        myCharacter = character;
+        characterStats = myCharacter.GetComponent<TemporaryStats>();  // Update the character stats when myCharacter is set
     }
 
-    // Called when the mouse pointer enters the UI element
     public void OnPointerEnter(PointerEventData eventData)
     {
         if (characterStats != null)
         {
-            // Update the HP and AP TextMesh Pro components separately
             hpText.text = characterStats.CurrentHealth.ToString();
             apText.text = characterStats.CurrentAP.ToString();
 
-            // Make sure both text objects are visible
             hpText.gameObject.SetActive(true);
             apText.gameObject.SetActive(true);
         }
     }
 
-    // Called when the mouse pointer exits the UI element
     public void OnPointerExit(PointerEventData eventData)
     {
-        // Hide the HP and AP text when not hovering
         hpText.gameObject.SetActive(false);
         apText.gameObject.SetActive(false);
     }
