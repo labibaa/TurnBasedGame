936398797fc2b992b7b739fe7cf803a55a54a2db
NUSHRAT <nushrat.j01@gmail.com>
2024-10-22 17:41:42 +0600

    tpc

--- Assets/_Mother Project/Scripts/Character/Player Companions/PlayerCompanions.cs
+++ Assets/_Mother Project/Scripts/Character/Player Companions/PlayerCompanions.cs
@@ -1,26 +1,42 @@
+using StarterAssets;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.InputSystem;
 
 public class PlayerCompanions : MonoBehaviour
 {
     public Transform mainPlayer;
     public float maxDistance = 1f;
     public float maxTime = 0.8f;
+    float timer = 0f;
+    bool linkUp = false;
+    bool isMainCharacter;
 
     NavMeshAgent agentCompanion;
     Animator animator;
-    float timer = 0f;
-    bool linkUp = false;
+    ThirdPersonController thirdPersonController;
+    PlayerInput playerInput;
+ 
     private void Start()
     {
         agentCompanion = GetComponent<NavMeshAgent>();
         animator = GetComponent<Animator>();
+        animator.SetFloat("MotionSpeed", 1);
+        thirdPersonController = GetComponent<ThirdPersonController>();
+        playerInput = GetComponent<PlayerInput>();
+
     }
     private void Update()
     {
+        LinkUp();
         FollowPlayer();
+      
+    }
+
+    public void LinkUp()
+    {
         if (Input.GetKeyDown(KeyCode.L))
         {
             if (!linkUp)
@@ -52,5 +68,19 @@ public class PlayerCompanions : MonoBehaviour
             animator.SetFloat("Speed", agentCompanion.velocity.magnitude);
         }
       
+    }
+
+    public void CharacterSwitch()
+    {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            if(isMainCharacter)
+            {
+                thirdPersonController.enabled = false;
+                playerInput.enabled = false;
+            }
+           
+        }
+            
     }
 }
