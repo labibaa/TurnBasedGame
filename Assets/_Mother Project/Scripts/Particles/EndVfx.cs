a49982dd5a407cb44289b61998225f6b93ffae28
Imtiaz Kabir <82272790+freeleech9@users.noreply.github.com>
2024-12-03 16:20:49 +0600

    skeleton grab

---
+++ Assets/_Mother Project/Scripts/Particles/EndVfx.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class EndVfx : MonoBehaviour
+{
+    public List<VisualEffect> effect;
+
+    public void EndVfxKey()
+    {
+        Debug.Log("EndVFXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+        if (effect != null)
+        {
+            foreach (VisualEffect vis in effect)
+            {
+                Debug.Log("end");
+                vis.SendEvent("end");
+                vis.SendEvent("stop");
+                StartCoroutine(DestroyVFX(vis));
+            }
+            
+        }
+
+    }
+
+    public IEnumerator DestroyVFX(VisualEffect desEffect)
+    {
+        yield return new WaitForSeconds(1f);
+        Destroy(desEffect.gameObject);
+        //  Destroy(hiteffect.gameObject);
+        Debug.Log("destroyt");
+    }
+}
