69a6f9325186a04831e9bc4b79596be3654bb891
NushoRaat <nushrat.j01@gmail.com>
2024-12-15 16:42:48 +0600

    Revert "Merge branch 'TryOutBranch' of https://github.com/labibaa/TurnBasedRepository into TryOutBranch"
    This reverts commit 602aae980b4fca83b884f33335f93b2608e2bf2d, reversing
    changes made to 61d10b132ec0d68cd78640a3b118fa3b83930466.

--- Assets/_Mother Project/Scripts/Inventory System/Inventory_UI.cs
+++ Assets/_Mother Project/Scripts/Inventory System/Inventory_UI.cs
@@ -7,8 +7,6 @@ using UnityEngine.UI;
 
 public class Inventory_UI : MonoBehaviour
 {
-    public static Inventory_UI instance;
-
     [SerializeField] Transform inventoryItem_panel;
     [SerializeField] Button inventoryItem_buttonPrefab;
     [SerializeField] Transform details_Panel;
@@ -16,15 +14,6 @@ public class Inventory_UI : MonoBehaviour
     [SerializeField] Transform StoreItem_panel;
     [SerializeField] Transform StoreItemDetails_Panel;
     [SerializeField] GameObject storeitemDetailPanel_Prefab;
-  
-
-    private void Awake()
-    {
-        if (instance == null)
-        {
-            instance = this;
-        }
-    }
 
     public StoreObjects store;
     public void RefreshInventoryUI()
